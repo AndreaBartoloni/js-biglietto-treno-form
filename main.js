25aefba8f0a91445b57798b61ebacb91bdce9862
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-11-09 09:47:21 +0100

    commit

--- main.js
+++ main.js
@@ -11,33 +11,33 @@ btn.addEventListener("click", function(){
     const nomeCognome = document.getElementById("nome-cognome").value;
     const nomePasseggeroElement = document.getElementById("nome-passeggero");
     if( Number.isFinite(eta) && Number.isFinite(chilometri) ) {
+        // Calcola costo iniziale
+        let costo = chilometri * price;
+        
+        
+        // Sconto under 18
+        if(eta < 18) {
+            costo = costo * 0.8;
+            
+            message = `Hai diritto allo sconto junior. `;
     
-         // Calcola costo iniziale
-         let costo = chilometri * price;
-        
-        
-         // Sconto under 18
-         if(eta < 18) {
-             costo = costo * 0.8;
-                
-             message = `Hai diritto allo sconto junior. `;
-        
-         // Sconto over 65
-         } else if(eta >= 65) {
-             costo = costo * 0.6;      
-             message = `Hai diritto allo sconto senior. `;
-         }
-        
-         //Dopo if/else dell'età
-         message += `${costo.toFixed(2)} €`;
-        
-            // In caso di errori relativi ai dati inseriti
-    } else {
+        // Sconto over 65
+        } else if(eta >= 65) {
+            costo = costo * 0.6;      
+            message = `Hai diritto allo sconto senior. `;
+        }
+    
+        //Dopo if/else dell'età
+        message += `${costo.toFixed(2)} €`;
+    
+        // In caso di errori relativi ai dati inseriti
+            
+    }   else {
             message = "Errore: i dati inseriti non sono numerici";
         }
-    //console.log(nomeCognome);
-    element.innerHTML = message;
-    nomePasseggeroElement.textContent = nomeCognome;
+        //console.log(nomeCognome);
+        element.innerHTML = message;
+        nomePasseggeroElement.textContent = nomeCognome;
 });
 remove.addEventListener('click', function(){
     document.getElementById("eta").value = "";
